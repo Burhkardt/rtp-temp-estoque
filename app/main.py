0eec3740d1daa58541789ec69b7ad2c09a8edb0f
jeanwox <woxtfm@gmail.com>
2026-05-12 10:48:45 -0300

    bota no alto

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.auth.routes import router as auth_router
+
+app = FastAPI(
+    title="Projeto 3 API",
+    version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth_router)
+
+@app.get("/")
+def root():
+    return {
+        "message": "Projeto 3 Online"
+    }
